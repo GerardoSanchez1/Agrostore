c1d74bdd9564aa31107808fb19423792718cb2b1
GerardoSanchez1 <gerardo-sanchez-1@hotmail.com>
2026-08-19 14:30:28 -0500

    Agrego re_path y serve en urls.py

--- mystore/urls.py
+++ mystore/urls.py
@@ -2,7 +2,8 @@
 URL configuration for mystore project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
