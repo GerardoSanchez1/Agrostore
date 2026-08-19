7c9a420169a9204722917c4d6e0c75bb82117188
GerardoSanchez1 <gerardo-sanchez-1@hotmail.com>
2026-08-19 14:14:13 -0500

    Habilito la ruta de media para produccion

--- mystore/urls.py
+++ mystore/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('', include('store.urls')),
 ]
 
-# Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
